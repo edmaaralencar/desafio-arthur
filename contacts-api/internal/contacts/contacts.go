f01b3c8eb480ccf028bdeb93cce0078fede41fb8
Edmar Alencar <edmaaralencar1@gmail.com>
2025-08-11 23:00:43 -0300

    feat: create contact struct and store interface

---
+++ contacts-api/internal/contacts/contacts.go
@@ -0,0 +1,21 @@
+package contacts
+
+import (
+	"context"
+	"time"
+)
+
+type Contact struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	CpfCnpj  string `json:"cpf_cnpj"`
+	Phone    string `json:"phone"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Store interface {
+	List(ctx context.Context) ([]Contact, error)
+	ListPaginated(ctx context.Context, page, perPage int) ([]Contact, int, error)
+}
